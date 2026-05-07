d574e0d2499e08faadf67fc0ca7775b1fc5f1a18
retr0x-1 <92461107+retr0x-1@users.noreply.github.com>
2026-05-07 21:55:39 +0100

    reverted blog

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
-// import Blog from './components/Blog';
+import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -33,7 +33,7 @@ export default function Home() {
 
         <div className="section-divider" />
 
-        {/* <Blog /> */}
+        <Blog />
 
         <div className="section-divider" />
 
